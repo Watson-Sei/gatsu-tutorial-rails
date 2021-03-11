a67a31493975a48f37ef3f3947f85b18a9ba0299
Watson-Sei <seinabehack@gmail.com>
2021-03-11 23:22:25 +0900

    first commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,50 @@
+class ArticlesController < ApplicationController
+    before_action :set_article, only: [:show, :edit, :update, :destroy]
+    def index
+        @articles = Article.all.order(id: :desc)
+    end
+
+    def new 
+        @article = Article.new
+    end
+
+    def create 
+        @article = Article.new(article_params)
+
+        if @article.save
+            redirect_to article_path(@article_params)
+        else
+            render 'new'
+        end
+    end
+
+    def show
+    end
+
+    def edit 
+    end
+
+    def update
+        p '1111111111111'
+        if @article.update(article_params)
+            redirect_to article_path(@article_params)
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy
+        p 'aaaaaaaaaaaaaa'
+        @article.destroy
+        redirect_to articles_path
+    end
+
+    private
+        def article_params
+            params.require(:article).permit(:title, :text)
+        end
+
+        def set_article
+            @article = Article.find(params[:id])
+        end
+end
